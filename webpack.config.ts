6130c9f6828b58b744491a0018c9be0d44601c99
mlatysheva <mlatysheva@gmail.com>
2022-09-17 22:33:47 +0200

    init: set up webpack in dev mode

---
+++ webpack.config.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import webpack from 'webpack';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildPaths } from './config/build/types/config';
+
+const paths: BuildPaths = {
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  build: path.resolve(__dirname, 'build'),
+  html: path.resolve(__dirname, 'public', 'index.html'), 
+}
+
+const mode = 'development';
+const isDev = mode === 'development';
+const PORT = 3000;
+
+const config: webpack.Configuration = buildWebpackConfig({
+  mode: 'development',
+  paths,
+  isDev,
+  port: PORT,
+});
+
+export default config;
+
