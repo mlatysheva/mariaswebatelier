89560ee1d202ae78349e30742c53975b15870632
mlatysheva <mlatysheva@gmail.com>
2022-10-22 23:04:10 +0200

    feat: add error boundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { BugButton } from "../../../app/providers/ErrorBoundary/ui/BugButton";
 
 const MainPage = () => {
   const { t, i18n } = useTranslation();
   
   return (
     <div>
+      <BugButton />
       <h1>{t('home')}</h1>
     </div>
   );
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useTranslation } from "react-i18next";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from "shared/ui/Button/Button";
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string;
+}
+
+export const PageError = ({className}: PageErrorProps) => {
+  const { t } = useTranslation();
+
+  const goHome = () => {
+    window.location.href = '/';
+  };
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
+  
+  return (
+    <div className={classNames(cls.PageError, {}, [className || ''])}>
+      <p>{ t ('unforseenError') }</p>
+      <Button
+        onClick={reloadPage}
+      >
+        { t ('reloadPage') }
+      </Button>
+    </div>
+  )
+};
