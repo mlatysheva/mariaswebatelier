02526d858d0ea07d613e7eace6ad07e447a7c089
mlatysheva <mlatysheva@gmail.com>
2022-10-31 17:28:35 +0100

    fix: correct translations for storybook

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './app/App'
 import ErrorBoundary from "./app/providers/ErrorBoundary/ui/ErrorBoundary"
 import { ThemeProvider } from './app/providers/ThemeProvider'
-import i18n from './shared/config/i18n/i18n'
-console.dir(i18n);
+import './shared/config/i18n/i18n'
 
 render(
   <BrowserRouter>
