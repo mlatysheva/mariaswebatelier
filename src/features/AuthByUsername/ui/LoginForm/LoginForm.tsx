f708e359562337e3b5ba37145c66523096daf8de
mlatysheva <mlatysheva@gmail.com>
2022-11-18 23:39:07 +0100

    feat: add dynamic module loader with dynamic reducers

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -6,6 +6,7 @@ import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
 import { Text, TextTheme } from 'shared/ui/Text/Text';
 import { ReduxStoreWithManager } from 'app/providers/StoreProvider';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader';
 import { getLoginState } from '../../model/selectors/getLoginState';
 import { loginByUsername } from '../../model/services/loginByUsername';
 import { loginActions, loginReducer } from '../../model/slice/loginSlice';
@@ -19,25 +20,17 @@ export interface LoginFormProps {
   className?: string;
 }
 
+const initialReducers: ReducersList = {
+  loginForm: loginReducer,
+};
+
 const LoginForm = memo(({ className }: LoginFormProps) => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const store = useStore() as ReduxStoreWithManager;
   const username = useSelector(getLoginUsername);
   const password = useSelector(getLoginPassword);
   const isLoading = useSelector(getLoginIsLoading);
   const error = useSelector(getLoginError);
-
-  useEffect(() => {
-    store.reducerManager.add('loginForm', loginReducer);
-    dispatch({ type: '@INIT loginForm reducer' });
-
-    return () => {
-      store.reducerManager.remove('loginForm');
-      dispatch({ type: '@DELETE loginForm reducer' });
-    };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const onChangeUsername = useCallback((value: string) => {
     dispatch(loginActions.setUsername(value));
@@ -54,33 +47,36 @@ const LoginForm = memo(({ className }: LoginFormProps) => {
   }, [dispatch, password, username]);
 
   return (
-    <div className={classNames(cls.LoginForm, {}, [className || ''])}>
-      <Text title={t('authorisation_form')} />
-      { error && <Text text={error} theme={TextTheme.ERROR} />}
-      <Input
-        autofocus
-        type="text"
-        className={cls.input}
-        placeholder={t('enter_name')}
-        onChange={onChangeUsername}
-        value={username}
-      />
-      <Input
-        type="text"
-        className={cls.input}
-        placeholder={t('enter_password')}
-        onChange={onChangePassword}
-        value={password}
-      />
-      <Button
-        className={cls.loginBtn}
-        theme={ButtonTheme.OUTLINE}
-        onClick={onLoginClick}
-        disabled={isLoading}
-      >
-        { t('login') }
-      </Button>
-    </div>
+    // eslint-disable-next-line i18next/no-literal-string
+    <DynamicModuleLoader reducers={initialReducers} removeAfterUnmount>
+      <div className={classNames(cls.LoginForm, {}, [className || ''])}>
+        <Text title={t('authorisation_form')} />
+        { error && <Text text={error} theme={TextTheme.ERROR} />}
+        <Input
+          autofocus
+          type="text"
+          className={cls.input}
+          placeholder={t('enter_name')}
+          onChange={onChangeUsername}
+          value={username}
+        />
+        <Input
+          type="text"
+          className={cls.input}
+          placeholder={t('enter_password')}
+          onChange={onChangePassword}
+          value={password}
+        />
+        <Button
+          className={cls.loginBtn}
+          theme={ButtonTheme.OUTLINE}
+          onClick={onLoginClick}
+          disabled={isLoading}
+        >
+          { t('login') }
+        </Button>
+      </div>
+    </DynamicModuleLoader>
   );
 });
 
