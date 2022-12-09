b64a7eb0b4573fd0ef6f258bb0dba6e98a0a0cdf
mlatysheva <mlatysheva@gmail.com>
2022-12-09 22:59:52 +0100

    feat: add react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,32 +1,16 @@
 import { BuildOptions } from './types/config'
 import webpack from 'webpack'
 import { buildCssLoader } from "./loaders/buildCssLoader"
+import { buildBabelLoader } from './loaders/buildBabelLoader'
 
-export function buildLoaders ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {
+  const { isDev } = options;
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack']
   }
 
-  const babelLoader = {
-    test: /\.(js|jsx|tsx|ts)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env'],
-        // plugins: [
-        //   [
-        //     'i18next-extract',
-        //     {
-        //       locales: ['en', 'ru'],
-        //       keyAsDefaultValue: false
-        //     }
-        //   ]
-        // ]
-      }
-    }
-  }
+  const babelLoader = buildBabelLoader(options);
 
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff|woff2|eot|ttf|otf)$/i,
