3b084503351246469759b22efa2d2568c4b5b73a
mlatysheva <mlatysheva@gmail.com>
2022-12-11 22:37:14 +0100

    feat: add articles and article details pages

---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import 'app/styles/index.scss';
+import { Theme } from 'app/providers/ThemeProvider/lib/ThemeContext';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
+import ArticlesPage from './ArticlesPage';
+
+export default {
+  title: 'pages/ArticlesPage',
+  component: ArticlesPage,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof ArticlesPage>;
+
+const Template: ComponentStory<typeof ArticlesPage> = () => <ArticlesPage />;
+
+export const Light = Template.bind({});
+Light.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
--- src/widgets/Sidebar/model/items.ts
+++ src/widgets/Sidebar/model/items.ts
@@ -3,6 +3,7 @@ import { RoutePath } from 'shared/config/routerConfig/routerConfig';
 import AboutIcon from 'shared/assets/icons/about-woman.svg';
 import HomeIcon from 'shared/assets/icons/home.svg';
 import ProfileIcon from 'shared/assets/icons/profile-icon.svg';
+import ArticleIcon from 'shared/assets/icons/article-20-20.svg';
 
 export interface SidebarItemType {
   path: string;
@@ -26,6 +27,12 @@ export const SidebarItemsList: SidebarItemType[] = [
     path: RoutePath.profile,
     Icon: ProfileIcon,
     text: 'profile',
+    authOnly: true,
+  },
+  {
+    path: RoutePath.articles,
+    Icon: ArticleIcon,
+    text: 'articles',
     authOnly: true,
   },
 ];
