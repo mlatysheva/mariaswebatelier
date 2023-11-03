7a1984dfe846615fb11e4ac08b0a5d9c42ccaa0c
mlatysheva <mlatysheva@gmail.com>
2023-11-03 19:27:08 +0100

    feat: add create/edit article route and navbar button

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,9 +2,12 @@ import React, { memo, useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { getUserAuthData, userActions } from '../../../entities/User';
-import { LoginModal } from '../../../features/AuthByUsername';
-import { Button, ButtonTheme } from '../../../shared/ui/Button/Button';
+import { getUserAuthData, userActions } from 'entities/User';
+import { LoginModal } from 'features/AuthByUsername';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Text, TextTheme } from 'shared/ui/Text/Text';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { RoutePath } from 'shared/config/routerConfig/routerConfig';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -32,6 +35,18 @@ export const Navbar = memo(({ className }: NavbarProps) => {
   if (authData) {
     return (
       <header className={classNames(cls.Navbar, {}, [className || ''])}>
+        <Text
+          className={cls.appName}
+          title={t('maria_web')}
+          theme={TextTheme.PRIMARY}
+        />
+        <AppLink
+          to={RoutePath.article_create}
+          theme={AppLinkTheme.SECONDARY}
+          className={cls.createBtn}
+        >
+          {t('create_article')}
+        </AppLink>
         <Button
           theme={ButtonTheme.OUTLINE}
           className={cls.links}
